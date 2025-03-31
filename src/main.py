b1097accda516789ef9149530a7b811092589bc4
Matti10 <mattias.winsen@outlook.com.au>
2025-03-31 22:59:35 +1000

    CAN WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main.py
+++ src/main.py
@@ -1,11 +1,53 @@
-import RCU
-from static import *
-import asyncio
+# import RCU
+# from static import *
+# import asyncio
 
-rcu = RCU.RCU()
+# rcu = RCU.RCU()
 
-sl = rcu.add_RCUFunc(SHIFTLIGHT_TYPE)
-sl = rcu.add_RCUFunc(RPMREADER_TYPE)
-asyncio.run(rcu.init_all_RCUFuncs())
+# sl = rcu.add_RCUFunc(SHIFTLIGHT_TYPE)
+# sl = rcu.add_RCUFunc(RPMREADER_TYPE)
+# asyncio.run(rcu.init_all_RCUFuncs())
 
-rcu.export_config(configPath="/workspaces/Resu-Control-Unit/src/data/test-config.json")
+# rcu.export_config(configPath="/workspaces/Resu-Control-Unit/src/data/test-config.json")
+
+import uasyncio as asyncio
+import CAN
+import time
+
+dev = CAN(0, extframe=False, tx=22, rx=19, mode=CAN.LOOPBACK, baudrate=500000, auto_restart=False)
+
+# - identifier of can packet (int)
+# - extended packet (bool)
+# - rtr packet (bool)
+# - data frame (0..8 bytes)
+
+def run():
+    i = 0
+    while True:
+        log("-")
+        if dev.any():
+            data = dev.recv()
+            # log(f"id:{hex(data[0])}, ex:{data[1]}, rtr:{data[2]}, data:{data[3]}")
+            log("e")
+        # await asyncio.sleep(0.01)
+        time.sleep(0.4)
+
+        i += 1
+        if i == 10:
+            log("send ACK")
+            dev.send([0,0,0,0,0,0,0,0], 0x305)
+            i = 0
+
+        
+def log(str):
+    with open("log.txt", "w") as f:
+        f.write(f"{str}\n")
+        
+def print_log():
+    # Open the file in read mode ('r')
+    with open("log.txt", "r") as file:
+        # Read the entire content of the file
+        content = file.read()
+
+    # Print the content
+    print(content)
